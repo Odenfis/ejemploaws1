135fef9fce853eea2bc0bb7462cae99789860f03
Odenfis <105258251+Odenfis@users.noreply.github.com>
2022-05-10 09:02:49 -0500

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const port = 3000;  
 
-const server = http.createserver((req, res) => {
-    res.statuscode = 200;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
     res.end('Hola a todos les dice su docente');
 });
